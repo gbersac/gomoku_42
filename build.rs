006761d81d77dfcccdf6e6966438c76425d23ff5
Guillaume Bersac <bersac_1@hotmail.fr>
2016-03-11 17:13:38 +0100

    delete .rust file

--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::env;
-
-fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    println!("cargo:rustc-link-search=native=.rust/lib/x86_64-apple-darwin");
-    // println!("cargo:rustc-link-lib=static=hello");
-}
